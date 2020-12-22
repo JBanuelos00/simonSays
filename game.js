96d2cb83587fefbff2265ec1b4008d043a890fbe
Javier Banuelos <J.Banuelos00@gmail.com>
2020-12-22 14:27:02 -0800

    Added in a check function.
    Created a function that would check the userPattern array versus the gamePattern array. Currently it only provides a sound if the preset arrays are not identical. The functionality to be completed are as follows:
    
    1) Loop through each array with each new user input up to gamePattern.length.
    2) Verify the inputs are identical.
    3) If any input does not match, immediately end the loop, declare incorrect and display game over
    4) If all inputs match, play nextSequence function

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 // Initialize variables required
 const btnColors = ["red", "blue", "green", "yellow"];
-const gamePattern = [];
-const userPattern = [];
 
+let gamePattern = [];
+let userPattern = [];
 let started = false;
 let randomChosenColor;
 let level;
@@ -32,7 +32,7 @@ function nextSequence() {
   // generate a random number between 0-3
   let randomNumber = Math.floor(Math.random() * 4);
 
-  level = 1;
+  level = 0;
 
   $("#level-title").text("Level " + level);
   // Now set a random color from the colors array using the random numbers
@@ -40,22 +40,28 @@ function nextSequence() {
   // Push the random color into the empty gamePattern array
   gamePattern.push(randomChosenColor);
 
-  for(i = 0; i < gamePattern.length; i++) {
+  for(let i = 0; i < gamePattern.length; i++) {
     let color = gamePattern[i];
     arrayLoop(color, i);
   }
 
   level++
-  console.log(level);
-
-  // console.log(gamePattern);
-
 }
 
 // function to check the answer of the user against the generated gamePattern
-function checkAnswer() {
+function checkAnswer(gamePattern, userPattern) {
   // take user input array and compare vs gamePattern
-  //give WRONG if a input is provided that doesn't match
+  for(let i = 0; i < userPattern.length; i++ ) {
+    if(userPattern[i] !== gamePattern[i]) {
+      //give WRONG if a input is provided that doesn't match
+      console.log("WRONG!");
+      playSound("wrong");
+      return reset();
+    } else {
+       nextSequence();
+       return userPattern = [];
+    }
+  }
 
   // call nextSequence() after 1000ms, empty user array.
 }
@@ -86,4 +92,16 @@ function playSound(name) {
   // Allow the sound to be set dependent on which btn was clicked
   let btnSound = new Audio("sounds/" + name +".mp3");
   btnSound.play();
+}
+
+function reset() {
+  // Display Game over
+  $("#level-title").text("Game Over");
+  // After delay clear all arrays and reset the level-title
+  setTimeout(function () {
+    started = false;
+    gamePattern = [];
+    userPattern = [];
+    $("#level-title").text("Press A Key to Start");
+  }, 2000);
 }
